ae5fe8e32b736b9eea6c1a1b05e1625fe54d2d30
Devin Warrick <dev.warr@gmail.com>
2019-06-26 17:17:57 -0500

    Matching navbar styling with UI page.

--- wedding-planner/src/components/navBar/NavBar.js
+++ wedding-planner/src/components/navBar/NavBar.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout, loginFix } from "../../actions";
+import "./NavBar.scss";
 
 
 // Links need logic to display or not display depending on whether user is logged in.
@@ -19,22 +20,22 @@ class NavBar extends React.Component{
         if (this.props.loggedBoolean) {
             const id = localStorage.getItem("userId")
             return (
-                <nav>
+                <nav className="navbar">
                     <img src="" alt="WeddingGram Logo" />
-                    <a href="#">Learn More</a>
-                    <Link to="/">Home</Link>
-                    <Link to={`/dashboard/${id}`}>Dashboard</Link>
-                    <Link onClick={this.props.logout} to="/">Logout</Link>
+                    <a href="https://wedinggram.netlify.com">LEARN MORE</a>
+                    <NavLink activeClassName="active" to="/">HOME</NavLink>
+                    <NavLink activeClassName="active"  to={`/dashboard/${id}`}>DASHBOARD</NavLink>
+                    <NavLink activeClassName="active"  onClick={this.props.logout} to="/">LOGOUT</NavLink>
                 </nav>
             )
         } else {
             return (
-                <nav>
+                <nav className="navbar">
                     <img src="" alt="WeddingGram Logo" />
-                    <a href="#">Learn More</a>
-                    <Link to="/">Home</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
+                    <a href="https://wedinggram.netlify.com">LEARN MORE</a>
+                    <NavLink activeClassName="active"  to="/">HOME</NavLink>
+                    <NavLink activeClassName="active"  to="/login">LOGIN</NavLink>
+                    <NavLink activeClassName="active"  to="/register">REGISTER</NavLink>
                 </nav>
             );
         }
